7d77bcf895a6498e29ba1d0ea15993181ac42adf
pogrebitskiy <pogrebitskiy.david@gmail.com>
2023-02-03 19:24:15 -0500

    finished assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+// when the button is clicked, the document is selected and dark mode is toggled
+function buttonClicked() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
